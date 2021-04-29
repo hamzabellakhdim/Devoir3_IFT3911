612124c8dd17262a44efe98bf5d9729a71aed9d1
bachwilliam17 <bachwilliam17@gmail.com>
2021-04-29 14:10:32 -0400

    nothing interesting

--- Code/ClientGestion.java
+++ Code/ClientGestion.java
@@ -1,11 +1,16 @@
 import java.util.Random;
+import java.util.ArrayList; // import the ArrayList class
+
 
 public class ClientGestion implements Gestionnaire {
+	ArrayList<Client> clients;
 
 
 	public ClientGestion() {
 		ModifClient observateur = new ModifClient();
 		attach(observateur);
+
+		this.clients = new ArrayList<Client>();
 	}
 
 	public Client ajouter(String adresse, String courriel, String tel, String dateNaissance, String numPasseport, String expPasseport) {
@@ -16,15 +21,23 @@ public class ClientGestion implements Gestionnaire {
 		int result = r.nextInt(high-low) + low;
 		String id = Integer.toString(result);
 
-		return new Client(id, adresse, courriel, tel , dateNaissance, numPasseport, expPasseport);
+		Client newClient = new Client(id, adresse, courriel, tel , dateNaissance, numPasseport, expPasseport);
+		this.clients.add(newClient);
+
+		return newClient;
 	}
 
 	public void supprimer(String id) {
-		
+		for (Client c : this.clients) {
+			if (c.getId().equals(id)) {
+				System.out.println("Client numéro " + id + "supprimé.");
+				this.clients.remove(c);
+			}
+		}
+		System.out.println("Aucun client avec cet ID.")
 	}
 
 	public void modifier(String id) {
-		
 	}
 
 	public void notify_() {
--- Code/Main_View.java
+++ Code/Main_View.java
@@ -5,39 +5,39 @@ public class Main_View {
 	/**
 	 * 
 	 * @param args
-	 * 
 	 */
 	public static void main(String[] args) {
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n================================================================================================================");
-		System.out.println("\n\nBienvenue dans le meilleur logiciel de réservation du cours IFT3911 !\n");
+		while (true) {
+			System.out.println("\n================================================================================================================");
+			System.out.println("\n\nBienvenue dans le meilleur logiciel de réservation du cours IFT3911 !\n");
 
-		System.out.println("Tapez 'c' si vous êtes un client.");
-		System.out.println("Tapez 'a' si vous êtes un admin.");
-		System.out.println("\n\n(non on a pas utilisé de switch case o_O)\n\n");
+			System.out.println("Tapez 'c' si vous êtes un client.");
+			System.out.println("Tapez 'a' si vous êtes un admin.");
+			System.out.println("\n\n(non on a pas utilisé de switch case o_O)\n\n");
 
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    	String response = myObj.nextLine();
+			Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+			String response = myObj.nextLine();
 
-		switch(response) {
-			case "c" : 
-				ClientView client = new ClientView();
-				client.afficherOptions();
-				break;
+			switch(response) {
+				case "c" : 
+					ClientView client = new ClientView();
+					client.afficherOptions();
+					break;
 
-			case "a" :
-				System.out.println("Veuillez entrer le mot de passe.");
-				response = myObj.nextLine();
-				System.out.println("\n\n\n\nDu premier coup !\n\n");
+				case "a" :
+					System.out.println("Veuillez entrer le mot de passe.");
+					response = myObj.nextLine();
+					System.out.println("\n\n\n\nDu premier coup !\n\n");
 
-				AdminView admin = new AdminView();
-				admin.afficherCRUD();
-				break;
-			
-			default :
-				System.out.println("Veuillez taper soit 'a' soit 'c'.");
-				break;
+					AdminView admin = new AdminView();
+					admin.afficherCRUD();
+					break;
+				
+				default :
+					System.out.println("Veuillez taper soit 'a' soit 'c'.");
+					break;
+			}
 		}
-
 	}
 
 }
